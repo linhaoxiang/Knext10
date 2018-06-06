3f2a902ef7f41903510c492b8b7c68124f3d2167
LinQ <linhaoxiang.tw@gmail.com>
2018-06-07 03:29:35 +0800

    anvbar update

--- model/navbar.php
+++ model/navbar.php
@@ -5,15 +5,17 @@
             </div>
             <div class="menuBG"></div>
             <ul class="nav" id="menu">
-                <li class="nav-item lang"><a href="" class="al">al</a> / <a href="" class="en">en</a></li>
-                <li class="nav-item menu-item1"><a class="nav-link" onclick="pageScrollTo('.s2');">About</a></li>
-                <li class="nav-item menu-item2"><a class="nav-link" onclick="pageScrollTo('.s3');">10 Things</a></li>
-                <li class="nav-item menu-item3"><a class="nav-link" onclick="pageScrollTo('.s4');">Future Vote</a></li>
-                <li class="nav-item menu-item4"><a class="nav-link" onclick="pageScrollTo('.s5');">Childrem</a></li>
-                <li class="nav-item menu-item5"><a class="nav-link" onclick="pageScrollTo('.s6');">Im Kosover</a></li>
-                <li class="nav-item menu-item6"><a class="nav-link" onclick="pageScrollTo('.s7');">K missions</a></li>
-                <li class="nav-item menu-item7"><a class="nav-link" onclick="pageScrollTo('.sponsers');">sponsers</a></li>
+            <li class="nav-item menu-item1"><a class="nav-link" href="">About</a></li>
+                <li class="nav-item menu-item2"><a class="nav-link" href="">Kosovo Wall</a></li>
+                <li class="nav-item menu-item3"><a class="nav-link" href="">Future Vote</a></li>
+                <li class="nav-item menu-item4"><a class="nav-link" href="">Child’s dream</a></li>
+                <li class="nav-item menu-item5"><a class="nav-link" href="">Im Kosover</a></li>
+                <li class="nav-item menu-item6"><a class="nav-link" href="">K missions</a></li>
+                <li class="nav-item menu-item5"><a class="nav-link" href="">Im Kosover</a></li>
+                <li class="nav-item menu-item6"><a class="nav-link" href="">Travel VR</a></li>
+                <li class="nav-item menu-item7"><a class="nav-link" href="">Partners</a></li>
                 <li class="space"></li>
+                <li class="nav-item lang"><a href="" class="al">al</a> / <a href="" class="en">en</a></li>
                 <li class="social-links"><i class="fab fa-instagram"><a href="google.com"></a></i></li>
                 <li class="social-links"><i class="fab fa-facebook-f"><a href="google.com"></a></i></li>
             </ul>
