cb21b605fea64e04d37a5aa353b47c18cdf364dd
LinQ <linhaoxiang.tw@gmail.com>
2018-06-07 03:31:17 +0800

    S1 update

--- model/s1.php
+++ model/s1.php
@@ -1,16 +1,15 @@
 <session class="sessions s1 row">
-                <div class="col-9"><img src="img/s01_BG.png" alt=""></div>
-                <div class="col-3 mainInfo">
+                <div class="col-12 col-md-9 KV"><img src="img/s01_BG.png" alt=""></div>
+                <div class="col-12 col-md-3 mainInfo">
                     <h6>Date:</h6>
-                    <h4>2018.06.27 - 07.22</h4>
+                    <h4>June 27- July 22, 2018</h4>
                     <h6>Venue:</h6>
-                    <h4>Kosovo Museum / Lapidarium /
-                            St.Nazim Gafurri, Pristina</h4>
+                    <h4>Kosovo Museum, Lapidarium</h4><p>St.Nazim Gafurri, Pristina</p>
                     <h6>Opening Hours</h6>
-                    <h4>11:00 - 19:00</h4>
+                    <h4>11:00-19:00</h4><p> (Closed on Monday)</p>
                     <h6>Organizer:</h6>
-                    <h4>Taiwan and Kosovo Cultural Exchange Association</h4>
-                    <h6>CO-Organizer:</h6>
-                    <h4>Kosovo Museum</h4>
+                    <h4>Taiwan and Kosovo Cultural Exchange Associaiton</h4>
+                    <h6>Co-organizers:</h6>
+                    <h4>Kosovo Museum, Termokiss, Zombie Soup</h4>
                 </div>
 </session>
