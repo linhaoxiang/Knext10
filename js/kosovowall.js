35f4b850c7acb18ab07d1244dce17fc131efdd13
Qmac <q@mixxin.tw>
2018-06-08 22:23:53 +0800

    s01 change title

---
+++ js/kosovowall.js
@@ -0,0 +1,20 @@
+// $(".options").click(
+//   knowing();
+// )
+$(".onbaby").css("back","block");
+
+// $(".s3btn").click(
+//   function(){
+//     $('#knowing').css("transform","translateX(0%)");
+//   }
+);
+
+// function knowing(){
+//     $('#knowing').css("transform","translateX(0%)");
+//     // $('.knowing-content').text($(this.attr("data"));
+// }
+
+function back(){
+    $('#knowing').css("transform","translateX(-105%)");
+}
+
